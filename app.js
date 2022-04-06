dccf10353171edca0447959f5ac5c0446142acd8
Said <slaoualy@yahoo.fr>
2022-04-06 17:55:50 +0200

    added a few functionnalities

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const drawingSpace = document.querySelector('.container');
-const clearBtn = document.querySelector('.clear-btn')
-
-
+const clearBtn = document.querySelector('.clear-btn');
+let squareSize = document.querySelector('.square-size');
 let square;
+let squareNumbers = 16;
+let mouseDown = false 
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+
+
+function changeSquareSize (e) {
+  squareNumbers = e.target.value;
+  // createSquares();
+  
+}
 
 function createSquares () {
-  for(let i = 0; i < 16 * 16; i++){
+  for(let i = 0; i < squareNumbers * squareNumbers; i++){
     square = document.createElement('div')
     square.className = 'square';
     drawingSpace.appendChild(square);
@@ -16,12 +26,12 @@ createSquares()
 
 const squares = document.querySelectorAll('.square');
 
+
 function fillSquare (e) {
-  
+  if(e.type === 'mouseover' && mouseDown === true) {
     e.target.classList.add('fill-square')
-  
-  
-  // squares.forEach(square => square.classList.add('fill-square'))
+  }
+    
 }
 
 function clearAll (e) {
@@ -30,11 +40,13 @@ function clearAll (e) {
   })
 }
 
-// fillSquare();
+
 
 squares.forEach(square => square.addEventListener('mouseover', fillSquare))
+squares.forEach(square => square.addEventListener('mousedown', fillSquare))
 
 clearBtn.addEventListener('click', clearAll)
 
+squareSize.addEventListener('input', changeSquareSize)
 
 
